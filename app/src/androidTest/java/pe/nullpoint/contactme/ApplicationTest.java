a82e42272a701f5f287f45edc3fcf48dab1212b7
ozzner <ozner_1619@hotmail.com>
2015-01-08 15:17:27 -0500

    El primer commit

---
+++ app/src/androidTest/java/pe/nullpoint/contactme/ApplicationTest.java
@@ -0,0 +1,14 @@
+package pe.nullpoint.contactme;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
